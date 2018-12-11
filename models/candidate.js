4282d13832141ff08aa357d9bf3a12d32fbb970e
Soullaft <soullaft@gmail.com>
2018-12-11 21:46:53 +0400

    init #2

--- models/candidate.js
+++ models/candidate.js
@@ -35,13 +35,5 @@ const candidateSchema = new Schema({
     }
 });
 
-// candidateSchema.methods.addToDb = function (url) {
-//     request("https://api.vk.com/method/users.get?user_ids=" + url + "&fields=bdate,city,country,home_town,photo_200,has_mobile,contacts,relation&access_token=" + token + "&v=5.92", (err, resp, body) => {
-//         const $ = cheerio.load(body);
-//         const result = JSON.parse($.text());
-//         return result;
-//     })
-// }
-
 
 module.exports = mongoose.model('Candidate', candidateSchema);
